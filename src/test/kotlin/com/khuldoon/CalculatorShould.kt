29d9cfbd549cbc987f7eb13472abca0ddc5dbc55
muhammad.sufyian <s.m.sufyian@gmail.com>
2024-09-08 15:57:48 +0200

    Comments added for the explanation

--- src/test/kotlin/com/khuldoon/CalculatorShould.kt
+++ src/test/kotlin/com/khuldoon/CalculatorShould.kt
@@ -4,10 +4,28 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 
+/***
+ * You should always think of behaviour when you are writing test.
+ * What is the behaviour which I am trying to validate and derive your
+ * code from the tests. If you evolve your code through this approach
+ * then you will also be getting the feedback about your design at the same
+ * time.
+ *
+ * This is referred to as Test driven development.
+ *
+ */
 class CalculatorShould {
 
     @Test
     fun `add numbers`() {
+        /**
+         * The fake implementation of the dependency of the Calculator is
+         * passed by implementing the interface in an inline fashion.
+         *
+         * The other approach could be by creating non-anonymous classes
+         * with the fake implementation of the dependencies and passing
+         * that to the calculator.
+         */
         val calculator = Calculator(object:Reader {
             override fun read(): List<Int> {
                 return listOf(2, 3)
@@ -19,17 +37,23 @@ class CalculatorShould {
 
     @Test
     fun `subtract numbers`() {
+        /**
+         * Look at the comment above for the explanation
+         */
         val calculator = Calculator(object:Reader{
             override fun read(): List<Int> {
                 return listOf(3, 2)
             }
         })
-        val result = calculator.substract()
+        val result = calculator.subtract()
         Assertions.assertEquals(1, result)
     }
 
     @Test
     fun `multiply numbers`() {
+        /**
+         * Look at the comment above for the explanation
+         */
         val calculator: Calculator = Calculator(object:Reader{
             override fun read(): List<Int> {
                 return listOf(5, 4)
@@ -41,6 +65,9 @@ class CalculatorShould {
 
     @Test
     fun `divide numbers`() {
+        /**
+         * Look at the comment above for the explanation
+         */
         val calculator = Calculator(object:Reader{
             override fun read(): List<Int> {
                 return listOf(20, 5)
@@ -53,44 +80,69 @@ class CalculatorShould {
 }
 
 
-
+/***
+ * The interfaces are subject to what is needed. What you want to provide as an abstraction.
+ * There are two schools , one is the role interface and the other is Header Interface.
+ * The practice are followed by different programming language communities.
+ *
+ * Read the article below to learn more about Role Interfaces.
+ * <href>https://www.martinfowler.com/bliki/RoleInterface.html</href>
+ *
+ * Read the article below to learn about Header Interfaces.
+ * <href>https://www.martinfowler.com/bliki/HeaderInterface.html</href>
+ *
+ */
 interface Reader {
     fun read():  List<Int>
 }
 
-
+/**
+ * TODO: Write code to take input from the file
+ */
 class FileReader : Reader {
     override fun read(): List<Int> {
-        //Some code which can read from the file
+        //code which can read from the file
         // and ond produce output
         return emptyList()
     }
 }
 
+/***
+ * TODO: Write code to take input from any Database
+ */
 class DatabaseReader: Reader {
     override fun read(): List<Int> {
         TODO("Not yet implemented")
     }
 }
 
-
-class InternetReader: Reader {
+/***
+ * TODO: Write code to take input from the api
+ */
+class RestAPIReader: Reader {
     override fun read(): List<Int> {
         TODO("Not yet implemented")
     }
 }
 
-
+/***
+ * Calculator is not depending on the implementation detail
+ * of the Reader but rather an interface which allows the calculator
+ * to change irrespective of the changes about where the input data is coming
+ * from.
+ *
+ * Coding to an interface also allows to write testable code. Now you can test
+ * calculator by writing fake implementations of the dependencies of the calculator.
+ *
+ */
 class Calculator(val reader: Reader) {
 
-    // Learn about List data structure
     fun add() : Int{
         val numbers = reader.read()
         return numbers.sum()
     }
 
-    // Learn about Arrays Datastructure
-    fun substract(): Int {
+    fun subtract(): Int {
         val numbers= reader.read()
         return numbers[0]-numbers[1]
     }
@@ -111,7 +163,7 @@ fun main() {
     val fileReader = FileReader()
     val fcalculator = Calculator(fileReader)
     fcalculator.add()
-    fcalculator.substract()
+    fcalculator.subtract()
     fcalculator.multiply()
     fcalculator.divide()
 
@@ -119,16 +171,16 @@ fun main() {
     val databaseReader = DatabaseReader()
     val dcalculator = Calculator(databaseReader)
     dcalculator.add()
-    dcalculator.substract()
+    dcalculator.subtract()
     dcalculator.multiply()
     dcalculator.divide()
 
-    val IReader= InternetReader()
+    val IReader= RestAPIReader()
     val iCalculator = Calculator(IReader)
     iCalculator.divide()
     iCalculator.multiply()
     iCalculator.add()
-    iCalculator.substract()
+    iCalculator.subtract()
 
 
 
